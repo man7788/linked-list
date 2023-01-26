be113fdae97ca8f7eb8741f3ba24087ff8742847
man7788 <jerman1841@gmail.com>
2023-01-26 23:34:56 +0800

    Add size method

--- src/index.js
+++ src/index.js
@@ -2,25 +2,29 @@ import './style.css';
 
 const LinkedList = () => {
   const list = {};
-  // Use recursion to find head or tail
+  // Use recursion to find tail
   const append = (value, next) => {
     if (next === undefined) {
       next = list;
     }
+
     if (next.nextNode === null) {
       next.nextNode = Node(value, null);
       return next;
     }
+
     if (Object.keys(list).length === 0) {
       const data = Node(value, null);
       list.value = data.value;
       list.nextNode = data.nextNode;
       return list;
     }
+
     if (typeof next.nextNode === 'object') {
       next = next.nextNode;
       return append(value, next);
     }
+
     return list;
   };
 
@@ -31,18 +35,39 @@ const LinkedList = () => {
       list.nextNode = data.nextNode;
       return list;
     }
+
     if (typeof list.nextNode === 'object') {
       const temp = { ...list };
       list.value = value;
       list.nextNode = temp;
-      console.log(list);
       return list;
     }
+
     return list;
   };
 
-  // Object.keys.length
-  const size = () => console.log('diu');
+  // Count how many recurions
+  const size = (next, total = 1) => {
+    if (next === undefined) {
+      next = list;
+    }
+
+    if (Object.keys(list).length === 0) {
+      return 0;
+    }
+
+    if (next.nextNode === null) {
+      return total;
+    }
+
+    if (typeof next.nextNode === 'object') {
+      next = next.nextNode;
+      return size(next, total + 1);
+    }
+
+    console.log('diu');
+    return total;
+  };
   // Recursion until nextNode.next
   const head = () => console.log('diu');
   // Recursion until nextNode is null
@@ -76,14 +101,15 @@ const LinkedList = () => {
 const Node = (value, nextNode) => ({ value, nextNode });
 
 const diu = LinkedList();
-diu.append(123);
-diu.append(456);
+// diu.append(123);
+// diu.append(456);
 // diu.append(999);
 
-diu.prepend('AAA');
-diu.prepend('BBB');
-diu.append(789);
+// diu.prepend('AAA');
+// diu.prepend('BBB');
+// diu.append(789);
 // diu.prepend('CCC');
 // diu.append('DDD');
 
 console.log(diu.list);
+console.log(diu.size());
